50a76606629724e6cd830f3302c9bc2e3102e83a
newptone <yuxcer@gmail.com>
2012-09-16 02:09:36 +0800

    Add com/con/param pp files

---
+++ manifests/params.pp
@@ -0,0 +1,12 @@
+class icclab::params{
+
+  #this contains the _basic_ common configuration
+  /* ---------------Important to set!------------------*/
+
+  $controller_node_address  = '192.168.56.3'
+  $nova_db_password = 'nova_pass'
+
+  /* --------------------------------------------------*/
+
+}
+
